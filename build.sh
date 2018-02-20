26eb9cb0748996f43ba77305ca4a4e48e195a0d4
Nate Smith <nate@stunphase.com>
2018-02-20 15:33:35 -0700

    Fix typo; change format;

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f index.html
-aglio --theme-variables slate --theme-full-width --theme-template triple -i api.apib -o index.html
+aglio --theme-variables slate --theme-full-width  -i api.apib -o index.html
